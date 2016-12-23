f21802506cd51f826729172ec299785d2b3c94d5
m4n3dw0lf <m4n3dw0lf@gmail.com>
2016-12-22 23:11:56 -0200

    Update

--- core/proxy.py
+++ core/proxy.py
@@ -133,7 +133,7 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         scheme, netloc, path = u.scheme, u.netloc, (u.path + '?' + u.query if u.query else u.path)
         assert scheme in ('http', 'https')
         if netloc:
-	    prefixes = ["wwww","waccounts","wmail","wbooks","wssl","wdrive","wmaps","wnews","wplay","wplus","wencrypted","wassets","wgraph","wfonts","wlogin","wsecure"]
+	    prefixes = ["wwww","waccounts","wmail","wbooks","wssl","wdrive","wmaps","wnews","wplay","wplus","wencrypted","wassets","wgraph","wfonts","wlogin","wsecure","wwiki"]
             req.headers['Host'] = netloc
 	    for prefix in prefixes:
 	    	if netloc.startswith(prefix):
